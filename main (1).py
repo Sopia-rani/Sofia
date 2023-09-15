f3e9587a4f2d8a68521468f3a99d4e55797f623e
Sopia-rani <144408035+Sopia-rani@users.noreply.github.com>
2023-09-15 09:51:28 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,14 +1,21 @@
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      # self.__account_balance = self.__account_balance+amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,
+                                                     self.__account_balance))
     else:
-        return False
-
-# Input year from the user
-year = int(input("Enter a year: "))
-
-# Check if it's a leap year
-if is_leap_year(year):
-    print(year, "is a leap year.")
-else:
-    print(year, "is not a leap year."
+      print("Invalid deposit amount.")
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__account_balance = self.__account_balance - amount
+      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
